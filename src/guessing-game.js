7dbf1a6a8d236a95ba67bbf9c4a7c8c4ccb2c8b1
kkolite <litavoregor@mail.ru>
2022-08-12 22:34:32 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.medium = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.medium = this.max - Math.floor((this.max - this.min) / 2);
     }
 
     guess() {
-
+        return this.medium;
     }
 
     lower() {
-
+        this.setRange(this.min, this.medium);
     }
 
     greater() {
-
+        this.setRange(this.medium, this.max);
     }
 }
 
